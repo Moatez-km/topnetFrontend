504f9d14690a52165b8bd6aedbcac563a37cfe00
moatez-km <moatez.kamounn@gmail.com>
2022-03-04 14:07:32 +0100

    add new user with abilities

--- src/components/admin/AddUser.js
+++ src/components/admin/AddUser.js
@@ -1,67 +1,90 @@
+import axios from "axios";
 import React, { useState } from "react";
+import swal from "sweetalert";
 import MasterLayout from "../../layouts/admin/MasterLayout";
+import { useNavigate } from "react-router-dom";
+
 function AddUser() {
+  const navigate = useNavigate();
   const [userInput, setUserInput] = useState({
     name: "",
     email: "",
     password: "",
-    role: "",
+    role_as: "",
+    error_list: [],
   });
+
   const handleInput = (e) => {
     e.persist();
     setUserInput({ ...userInput, [e.target.name]: e.target.value });
   };
+
   const submitUser = (e) => {
-    e.persist();
-    const data = {};
+    e.preventDefault();
+    const data = {
+      name: userInput.name,
+      email: userInput.email,
+      password: userInput.password,
+      role_as: userInput.role_as,
+    };
+    axios.post("/api/store-user", data).then((res) => {
+      if (res.data.status === 200) {
+        swal("Success", res.data.message, "success");
+
+        navigate("/admin/dashboard");
+      } else if (res.data.status === 400) {
+        setUserInput({ ...userInput, error_list: res.data.errors });
+      }
+    });
   };
 
   return (
     <MasterLayout>
       <div className="container-fluid px-4">
         <h2>Add New User</h2>
-        <form onSubmit={submitUser}>
-          <ul className="nav nav-tabs" id="myTab" role="tablist">
-            <li className="nav-item" role="presentation">
-              <button
-                className="nav-link active"
-                id="home-tab"
-                data-bs-toggle="tab"
-                data-bs-target="#home"
-                type="button"
-                role="tab"
-                aria-controls="home"
-                aria-selected="true"
-              >
-                Home
-              </button>
-            </li>
-            <li className="nav-item" role="presentation">
-              <button
-                className="nav-link"
-                id="profile-tab"
-                data-bs-toggle="tab"
-                data-bs-target="#profile"
-                type="button"
-                role="tab"
-                aria-controls="profile"
-                aria-selected="false"
-              >
-                Profile
-              </button>
-            </li>
-          </ul>
-          <div className="tab-content" id="myTabContent">
-            <div
-              className="tab-pane  card-body border fade show active"
-              id="home"
-              role="tabpanel"
-              aria-labelledby="home-tab"
+
+        <ul className="nav nav-tabs" id="myTab" role="tablist">
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active"
+              id="home-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#home"
+              type="button"
+              role="tab"
+              aria-controls="home"
+              aria-selected="true"
             >
-              <br></br>
-              <br></br>
+              Home
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link"
+              id="profile-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#profile"
+              type="button"
+              role="tab"
+              aria-controls="profile"
+              aria-selected="false"
+            >
+              Profile
+            </button>
+          </li>
+        </ul>
+        <div className="tab-content" id="myTabContent">
+          <div
+            className="tab-pane  card-body border fade show active"
+            id="home"
+            role="tabpanel"
+            aria-labelledby="home-tab"
+          >
+            <br></br>
+            <br></br>
+            <form onSubmit={submitUser} id="User_form">
               <div className="form-group mb-3">
-                <labal>Name</labal>
+                <label>Name</label>
                 <input
                   type="text"
                   name="name"
@@ -70,8 +93,9 @@ function AddUser() {
                   className="form-control"
                 />
               </div>
+              <span>{userInput.error_list.name}</span>
               <div className="form-group mb-3">
-                <labal>Email</labal>
+                <label>Email</label>
                 <input
                   type="email"
                   name="email"
@@ -79,9 +103,10 @@ function AddUser() {
                   value={userInput.email}
                   className="form-control"
                 />
+                <span>{userInput.error_list.email}</span>
               </div>
               <div className="form-group mb-3">
-                <labal>Password</labal>
+                <label>Password</label>
                 <input
                   type="password"
                   name="password"
@@ -89,20 +114,22 @@ function AddUser() {
                   onChange={handleInput}
                   value={userInput.password}
                 />
+                <span>{userInput.error_list.password}</span>
               </div>
               <div className="form-group mb-3">
-                <labal>Select our role</labal>
+                <label>Select our role</label>
                 <select
-                  class="form-select"
+                  className="form-select"
                   aria-label="Default select example"
                   onChange={handleInput}
-                  value={userInput.role}
+                  name="role_as"
+                  value={userInput.role_as}
                 >
-                  <option value="1">Admin</option>
-                  <option value="2">User</option>
-                  <option value="4">Encadrant</option>
-                  <option value="5">Chef departement</option>
-                  <option value="6">Service formation</option>
+                  <option value="admin">Admin</option>
+                  <option value="encadrant">Encadrant</option>
+                  <option value="user">User</option>
+                  <option value="chef departement">Chef departement</option>
+                  <option value="service formation">Service formation</option>
                 </select>
               </div>
               <div
@@ -113,13 +140,12 @@ function AddUser() {
               >
                 ...
               </div>
-            </div>
+              <button type="submit" className="btn btn-primary px-4 float-end">
+                Submit
+              </button>
+            </form>
           </div>
-
-          <button type="submit" className="btn btn-primary px-4 float-end">
-            Submit
-          </button>
-        </form>
+        </div>
       </div>
     </MasterLayout>
   );
